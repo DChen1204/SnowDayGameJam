17dd67bdceb8d1d5e9b3833af692d98eb2e0e660
HungBDao <bachung2005@gmail.com>
2024-11-11 22:48:15 -0500

    Change player positions and Updated Snowball behaviour

--- Assets/SnowFightGame/Snowball.cs
+++ Assets/SnowFightGame/Snowball.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Snowball : MonoBehaviour {
 
   private Rigidbody _rb;
+  [SerializeField] private float speedX = 10f;
+  [SerializeField] private float speedY = 5f;
 
   private void Awake() {
-    _rb = GetComponent<Rigidbody>();
-    _rb.velocity = new Vector3(5,5,0);
+      _rb = GetComponent<Rigidbody>();
+      _rb.velocity = new Vector3(speedX, speedY, 0);
   }
 
-  
+  private void OnCollisionEnter(Collision other) {
+      if (other.gameObject.CompareTag("Player")) {
+          Destroy(gameObject);
+      }
+  }
+
+  private void Update() {
+      if (transform.position.y < 2) {
+          Destroy(gameObject);
+      }
+  }
 
 }
